aedc158a0697541e42d6dad251a7f983d4a37b6d
Kyrylo_Kud <kudkiril@gmail.com>
2025-07-09 19:03:56 +0200

    removed map

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
 
 
-const numbers = [50, 49, 48, 47, 46, 45, 44, 120, 144, -138, -11, -33, 0068, -0094, null, undefined, true, 'kek'];
+const numbers = [50, 49, 48, 47, 46, 45, 44, 120, 144, -138, -11, -33, 0068, -0094, 0, null, undefined, true, 'kek'];
 
 const evenNum = numbers
 .filter(num => typeof num === 'number' && Number.isInteger(num)) // only numbers
-.map(num => Math.abs(num)) // removes - and 0
 .filter(num => num % 2 === 0); // only even numbers
 
 console.log(evenNum);
